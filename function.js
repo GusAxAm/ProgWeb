f6dd5e194a540a934df33fc4d6a27e16dd29bc1d
GusAxAm <112149352+GusAxAm@users.noreply.github.com>
2024-03-22 09:59:14 -0600

    Add files via upload

--- function.js
+++ function.js
@@ -1,26 +1,23 @@
-// language.js
-
-// Objeto con el contenido en diferentes idiomas
 var languageContent = {
     'es': {
         'welcome': '¡Bienvenido a nuestro hotel!',
         'description': 'Encuentra la mejor experiencia de hospedaje con nosotros.'
-        // Agrega más contenido en español aquí
+        
     },
     'en': {
         'welcome': 'Welcome to our hotel!',
         'description': 'Find the best lodging experience with us.'
-        // Add more English content here
+       
     }
 };
 
 // Función para cambiar el idioma de la página
 function changeLanguage(lang) {
     var content = languageContent[lang];
-    // Cambiar el contenido de acuerdo al idioma seleccionado
+    
     document.getElementById('welcome-message').innerText = content['welcome'];
     document.getElementById('description').innerText = content['description'];
-    // Cambiar cualquier otro contenido necesario
+   
 }
 
 
